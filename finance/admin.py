c7ca1937e3eff72e0634129397e44ab697dbe2f4
MahyarDev01 <mahyar.dev01@gmail.com>
2026-09-19 11:29:10 +0330

    update finance admin

--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import SubscriptionPlan, WalletTransaction, UserSubscription
 
-# Register your models here.
+admin.site.register(SubscriptionPlan)
+admin.site.register(WalletTransaction)
+admin.site.register(UserSubscription)

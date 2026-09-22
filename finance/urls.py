55f95cc593cad86c3acd2be24cfe013ac025b2d0
MahyarDev01 <mahyar.dev01@gmail.com>
2026-09-22 09:40:59 +0330

    update urls in finance app

--- finance/urls.py
+++ finance/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import WalletDashboardView, WalletDepositView, BuySubscriptionView
+from .views import WalletDashboardView, WalletDepositView, BuySubscriptionView, CancelSubscriptionView
 
 urlpatterns = [
     path('dashboard/', WalletDashboardView.as_view(), name='wallet-dashboard'),
     path('deposit/', WalletDepositView.as_view(), name='wallet-deposit'),
     path('buy-plan/<int:plan_id>/', BuySubscriptionView.as_view(), name='wallet-buy-plan'),
+    path('subscription/cancel/', CancelSubscriptionView.as_view(), name='cancel_subscription'), 
 ]
